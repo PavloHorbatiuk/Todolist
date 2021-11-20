ab9f2eb99d65ac52001f509bd6ec3977567735d3
PavloHorbatiuk <pavlo.horbatiuk@gmail.com>
2021-11-20 13:45:14 +0200

    Updates

---
+++ precache-manifest.0efe209b49b90621a071dbc6a9f6bee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5c0228c4df5d1bddb4cd9e5f2a38ef",
+    "url": "/PavloHorbatiuk/Todolist/index.html"
+  },
+  {
+    "revision": "429ffe545dd0b629e79b",
+    "url": "/PavloHorbatiuk/Todolist/static/css/main.9058b2c9.chunk.css"
+  },
+  {
+    "revision": "58749f5f38e94020a15f",
+    "url": "/PavloHorbatiuk/Todolist/static/js/2.62bc3f06.chunk.js"
+  },
+  {
+    "revision": "5d2ca75c67742da2b5594363196a95f1",
+    "url": "/PavloHorbatiuk/Todolist/static/js/2.62bc3f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429ffe545dd0b629e79b",
+    "url": "/PavloHorbatiuk/Todolist/static/js/main.1c0593ea.chunk.js"
+  },
+  {
+    "revision": "2eade01dc0b834700476",
+    "url": "/PavloHorbatiuk/Todolist/static/js/runtime-main.93dd1958.js"
+  }
+]);
